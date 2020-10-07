4673d30c3523872154fb639b523b715a0d4f620b
arain0918 <arain0918>
2020-10-07 23:19:44 +0900

    最初のコミット

---
+++ team-ecsite/src/main/java/jp/co/internous/teamecsite/TeamEcsiteApplication.java
@@ -0,0 +1,13 @@
+package jp.co.internous.teamecsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamEcsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamEcsiteApplication.class, args);
+	}
+
+}
